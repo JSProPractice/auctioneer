3e7ed87b13c3f3e1febb61b1170198952017eeef
Vipin <vipindixit2204@gmail.com>
2020-11-27 23:24:35 +0530

    CRUD end points created for auction, user and token

--- routes/api/auth.js
+++ routes/api/auth.js
@@ -1,20 +1,19 @@
 var express = require('express');
 var router = express.Router();
 
-
-router.post('/login',function(req, res, next){
+router.post('/login', function (req, res, next) {
     res.send('User loggged in successfully');
 });
 
-router.get('/logout',function(req, res, next){
+router.get('/logout', function (req, res, next) {
     res.send('User loggged out successfully');
 });
 
-router.post('/signup',function(req, res, next){
+router.post('/signup', function (req, res, next) {
     res.send('User signed up successfully');
 });
 
-router.get('/verify_email',function(req, res, next){
+router.get('/verify_email', function (req, res, next) {
     res.send('email verification done successfully');
 });
 

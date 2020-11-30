864b4620c36ef4444de64015214e91cfb12d0a64
Vipin <vipindixit2204@gmail.com>
2020-11-30 19:37:06 +0530

    service for auth route

--- routes/api/auth.js
+++ routes/api/auth.js
@@ -3,8 +3,14 @@ var router = express.Router();
 
 const userService = require('../../services/user');
 
-router.post('/login', function (req, res, next) {
-    res.send('User loggged in successfully');
+router.post('/login', async function (req, res, next) {
+    let email = req.body.email.trim().toLowerCase();
+    let password = req.body.password;
+    console.log('req.body',req.body);
+    console.log('email',email.length);
+
+    let response = await userService.loginCredentialsVerification(email, password);
+    res.send(response);
 });
 
 router.get('/logout', function (req, res, next) {
@@ -13,7 +19,7 @@ router.get('/logout', function (req, res, next) {
 
 router.post('/signup', async function (req, res, next) {
     let name = req.body.name;
-    let email = req.body.email;
+    let email = req.body.email.trim().toLowerCase();
     let password = req.body.password;
     let dob = req.body.dob;
 
@@ -22,8 +28,13 @@ router.post('/signup', async function (req, res, next) {
     res.send(response);
 });
 
-router.get('/verify_email', function (req, res, next) {
-    res.send('email verification done successfully');
+router.get('/verify_email', async function (req, res, next) {
+    let userId = req.query.userId;
+    let token = req.query.token;
+    console.log('req',req.query)
+    let response = await userService.veryfyEmail(userId, token);
+
+    res.send(response);
 });
 
 module.exports = router;
--- services/user.js
+++ services/user.js
@@ -1,4 +1,5 @@
 const mangoose = require('mongoose');
+const Token = mangoose.model('Token');
 const User = mangoose.model('User');
 
 module.exports = {
@@ -12,5 +13,40 @@ module.exports = {
             return 'Unable to save user';
         }
         return 'User is created';
+    },
+    loginCredentialsVerification: async function (email, password) {
+        try {
+            let user = await User.findOne({ 'email': email });
+            if (!user)
+                return 'User is not registered'
+            if (!user.isVerified)
+                return 'Please verify your email';
+            if (user.password !== password) {
+                return 'User login failied';
+            }
+
+        } catch (err) {
+            console.log(err);
+            return 'Unable to login user';
+        }
+
+        return 'User loggged in successfully';
+    },
+    veryfyEmail: async function (userId, token) {
+        try {
+            let tokenObj = await Token.findOne({ 'userId': userId });
+            console.log('tokenObj', tokenObj);
+            if (tokenObj == null)
+            //validTill
+            //delete token
+                return 'Token not found';
+            if (tokenObj.token != token) {
+                return 'Token verification failed';
+            }
+        } catch (err) {
+            console.log(err);
+            return 'Error occured in email verification. please try agian.'
+        }
+        return 'Email verified.'
     }
 };
